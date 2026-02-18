3d2159ad46be107bb80024f6d42357965b3edeb8
asadullahjamil0 <asadullahjamilchaudhry@gmail.com>
2026-02-18 23:14:13 +0500

    Added some routing.

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center ">
-      <Text className="text-3xl font-bold text-dark-100">Welcome to my Movie App</Text>
-      <Text className="font-bold text-accent">This is an amazing platform for latest movies</Text>
+      <Text className="text-3xl font-bold text-dark-100">Welcome to the Movie App</Text>
     </View>
   );
 }
